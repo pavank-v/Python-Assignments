8b1852744675c21c22813dc898f9e25c114eb57c
pavank1301 <130172796+pavank1301@users.noreply.github.com>
2024-05-26 09:55:52 +0530

    Delete Django/BookLibrary directory

--- Django/BookLibrary/library/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Book, SearchHistory
-
-admin.site.register(SearchHistory)
-admin.site.register(Book)
--- Django/BookLibrary/library/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import SearchHistory
-
-#Serializer for serializing the book details
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SearchHistory
-        fields = ['title', 'cover_image', 'amazon_url', 'flipkart_url', 'good_reads_review']
