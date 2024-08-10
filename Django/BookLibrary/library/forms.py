1db7857d9dfa7bf51fcf519e826ee1aee679c230
pavank1301 <130172796+pavank1301@users.noreply.github.com>
2024-08-10 21:20:23 +0530

    deleted django project

--- Django/BookLibrary/library/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-#Simple form to get the Book name from user
-class SearchBook(forms.Form):
-    title = forms.CharField(label='Search a book', 
-                            max_length=100, 
-                            widget=forms.TextInput(attrs={'placeholder': 
-                                'Enter any Book Name'}))
